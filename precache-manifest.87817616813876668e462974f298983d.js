14fbca4f8c6c86db5bbbf1f9f08098eefeacdc2f
Hwanyoung Jang <>
2020-03-14 11:38:03 +0900

    Updates

---
+++ precache-manifest.87817616813876668e462974f298983d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fc7b9662acedf093a2842dcd05cb4c8",
+    "url": "/mask-info/index.html"
+  },
+  {
+    "revision": "edf07305f43f56b862b6",
+    "url": "/mask-info/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "1a1c26f1a6d295a1744f",
+    "url": "/mask-info/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "edf07305f43f56b862b6",
+    "url": "/mask-info/static/js/2.201958c8.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/mask-info/static/js/2.201958c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a1c26f1a6d295a1744f",
+    "url": "/mask-info/static/js/main.31b66c66.chunk.js"
+  },
+  {
+    "revision": "12d608a0512d39460d18",
+    "url": "/mask-info/static/js/runtime-main.a9148be6.js"
+  }
+]);
